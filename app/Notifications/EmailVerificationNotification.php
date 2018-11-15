3d803375298c9220089750b999c326e41055fa67
amosforli <496370514@qq.com>
2018-11-15 22:15:20 +0800

    触发激活邮件

---
+++ app/Notifications/EmailVerificationNotification.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Str;
+use Cache;
+class EmailVerificationNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        // 生成随机字符串
+        $token = Str::random(16);
+        // 写入缓存 30分钟
+        Cache::set('email_verification_' . $notifiable->email, $token, 30);
+        $url = route('email_verification.verify', ['email' => $notifiable->email, 'token' => $token]);
+        return (new MailMessage)
+                    ->greeting($notifiable->name.'您好：') // 欢迎词
+                    ->subject('注册成功，请验证您的邮箱') // 邮件标题
+                    ->line('请点击下方链接验证您的邮箱') //添加一行文字
+                    ->action('验证', $url); // 添加一个链接按钮
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
