98b64b666c0c2683e0be4c4003814bc42bf10afe
amosforli <496370514@qq.com>
2018-11-21 17:06:15 +0800

    用户界面 - 购物车页面

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
+use App\Models\ProductSku;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+    public function index(Request $request)
+    {
+        $cartItems = $request->user()->CartItems()->with(['productSku.product'])->get();
+        return view('cart.index', ['cartItems' => $cartItems]);
+    }
     public function add(AddCartRequest $request)
     {
         $user = $request->user();
@@ -24,6 +30,12 @@ class CartController extends Controller
             $cart->productSku()->associate($skuId);
             $cart->save();
         }
+        return [];
+    }
+
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
         return [];
     }
 }
